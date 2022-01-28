5f9f2cb2f0746c744da2c72576230ad50afedae4
Женя <44874349+zhenya-mikhin@users.noreply.github.com>
2022-01-28 14:16:01 +0300

    поптыка запустить прод.версию на gh-pages

--- types/my-element.d.ts
+++ types/my-element.d.ts
@@ -2,7 +2,8 @@ import { LitElement } from 'lit';
 import './styles/index.scss';
 export declare class MyElement extends LitElement {
     static styles: import("lit").CSSResultGroup;
-    date: Date;
+    today: Date;
+    private _getToday;
     render(): import("lit-html").TemplateResult<1>;
 }
 declare global {
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,16 @@
 import { defineConfig } from 'vite'
 import litcss from 'rollup-plugin-postcss-lit'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      entry: 'src/my-element.ts',
-      formats: ['es']
-    },
-    rollupOptions: {
-      external: /^lit/
-    }
-  },
+  // build: {
+  //   lib: {
+  //     entry: 'src/my-element.ts',
+  //     formats: ['es']
+  //   },
+  //   rollupOptions: {
+  //     external: /^lit/
+  //   }
+  // },
   plugins: [
     {
       ...litcss(),
