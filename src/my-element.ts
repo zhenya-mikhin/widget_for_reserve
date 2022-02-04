3156e0ced029c15f2742128aaa287ca0aa6e8307
Женя <44874349+zhenya-mikhin@users.noreply.github.com>
2022-02-04 11:39:35 +0300

    исправил неправильные скобки в предыдущем коммите

--- src/my-element.ts
+++ src/my-element.ts
@@ -130,7 +130,7 @@ export class MyElement extends LitElement {
         </section>
 
         <label for="date-input" class="visually-hidden">date selection</label>
-        <input id=""date-input class="reserve-form__input-container"
+        <input id="date-input" class="reserve-form__input-container"
                  type="date" min="${this._getToday()}"
                  value="${this._getToday()}"
                  @change="${this._changeDate}"
